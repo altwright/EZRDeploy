d5264c3bfe181185a4af132b90b413437e667ac9
Blizzerac <TBlizzerac@hotmail.com>
2023-09-03 17:02:59 +0800

    simple fxns for handling input, to hook w UI

--- ioparsing.py
+++ ioparsing.py
@@ -1,4 +1,4 @@
-class Job:
+class Task:
     def __init__(self, name=None, desc=None, on_machine=False, file=None, file_name=None, args=None):
         self.name = name
         self.desc = desc
@@ -7,7 +7,7 @@ class Job:
         self.file_name = file_name
         self.args = args
     
-    # functions to append details to the job, maybe when wanting to edit job?
+    # functions to append details to the task, maybe when wanting to edit task?
     def add_name(self, name):
         self.name = name
     
@@ -29,21 +29,43 @@ class Job:
     
     def add_args(self, args):
         self.args = args
+    
+    def send_to_machine(self, computer, input=None):
+        # send the task to the computer
+        if (not input):
+            # call pypsexec here to send task to machine
+            pass
+        else:
+            # call pypsexec here to send whatever input to the machine
+            pass
 
 class TaskPage:
-    def __init__(self, job, computers):
-        self.job = job
+    def __init__(self, task, computers):
+        self.task = task
         self.computers = computers
-        pass
     
     def restart(self):
-        # take the job and computers and restart the job
+        # take the task and computers and restart the task
+        for computer in self.computers:
+            self.task.send_to_machine(computer)
         pass
     
     def cancel(self):
-        # take the job and computers and cancel the job
+        # take the task and computers and cancel the task
+        for computer in self.computers:
+            self.task.send_to_machine(computer, "cancel task to do")
         pass
     
-    def send_input(self, input):
+    def send_input(self, computer, input):
         # take computer and send input using pypsexec
+        self.task.send_to_machine(computer, input)
+        pass
+
+class TaskCreationPage:
+    def __init__(self):
+        self.task = Task() # details to be filled once create_task called
+    
+    def create_task(self):
+        # create the task
+        self.task.add_name(name.get()) # get name from tkinter, name is placeholder
         pass
