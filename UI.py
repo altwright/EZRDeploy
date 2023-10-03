75f44f3c4caf047ac321815df99fd08c983f086d
Benjamin Wright <allwrightyben@proton.me>
2023-10-03 03:04:39 -0700

    Untested: added task, job state.

--- UI.py
+++ UI.py
@@ -3,6 +3,10 @@ from tkinter import ttk
 from tab_content import ADTab, THTab, JCTab, create_grid, completedTab
 from typing import List
 from ioparsing import *
+from appstate import appState, JobState, TaskState
+from pypsexec.client import Client 
+from datetime import datetime
+from queue import Queue
 
 class TabManager:
     def __init__(self, tabFrame, contentFrame):
@@ -35,18 +39,67 @@ class TabManager:
             self.new_tab(name, job_path)
 
     def handle_JCTab(self, data):
-        print("creating a job called...." , data["NAME"])
-        print("with a program called....", data["PROGRAM"])
-        print("With the arguments...", data["ARGUMENTS"])
-        print("On the local machien....", data["LOCALMACHINE"])
-        print("Run as system admin....", data["SYSADMIN"])
-        print("With these additional Files....", data["ADDFILES"])
-        print("Created by..." , data["AUTHOR"])
-        print("With PCs:..." , data["PCs"])
+        task = TaskState()
+        task.name = data["NAME"]
+        task.author = data["AUTHOR"]
+        task.startDateTime = datetime.now()
+        task.programStr = data["PROGRAM"]
+        task.argsStr = data["ARGUMENTS"]
+        task.localProgram = data["LOCALMACHINE"]
+        task.localProgramSrcDir = data["LOCALSRC"]
+        if len(data["ADDFILES"]) > 0:
+            task.copyFiles = True
+        else:
+            task.copyFiles = False
+        task.copiedFilesList = data["ADDFILES"]
+        if data["WORKINGDIR"]:
+            task.remoteWorkingDir = data["WORKINGDIR"]
+        else:
+            task.remoteWorkingDir = None
+        task.impersonateSysAdmin = data["SYSADMIN"]
+        task.overwriteExe = data["OVERWRITE_EXE"]
+        task.overwriteFiles = data["OVERWRITE_FILES"]
+        task.cleanupExeAfterCopy = data["CLEANUP_EXE"]
+        task.cleanupFilesAfterCopy = data["CLEANUP_FILES"]
+        task.timeout = data["TIMEOUT"]
+
+        for pcName in data["PCs"]:
+            jobState = JobState()
+            jobState.clientName = pcName
+            jobState.client = Client(pcName)
+            jobState.stdinQ = Queue()
+            jobState.stdoutQ = Queue()
+            jobState.job = Job(
+                jobState.client,
+                task.programStr,
+                task.argsStr,
+                jobState.stdoutQ,
+                jobState.stdoutQ,
+                jobState.stdinQ,
+                copy_local_exe= task.localProgram,
+                local_exe_src_dir=task.localProgramSrcDir,
+                clean_copied_exe_after=task.cleanupExeAfterCopy,
+                overwrite_remote_exe=task.overwriteExe,
+                copy_local_files=task.copyFiles,
+                src_files_list=task.copiedFilesList,
+                overwrite_remote_files=task.overwriteFiles,
+                clean_copied_files_after=task.cleanupFilesAfterCopy,
+                use_system_account=task.impersonateSysAdmin,
+                working_dir=task.remoteWorkingDir,
+                timeout_seconds=task.timeout
+            )
+
+            task.jobList.append(jobState)
+
+            try:
+                jobState.client.connect()
+                jobState.client.create_service()
+                jobState.job.start()
+            except Exception as exc:
+                jobState.exc = exc
         
-        # replace with JCPage create_job_callback
-        print()
-    
+        appState.runningTasks.append(Task)
+
     #used to change tabs
     def show_content(self, content_frame, data_list):
         self.current_tab.remove_page()
--- appstate.py
+++ appstate.py
@@ -1,10 +1,42 @@
 from ms_active_directory.core.ad_objects import ADComputer
 from ms_active_directory.core.ad_session import ADSession
+from pypsexec.client import Client 
+from queue import Queue
+from job import Job
+from datetime import datetime
+
+class JobState:
+    clientName: str
+    client: Client
+    stdoutQ: Queue
+    stdinQ: Queue
+    job: Job
+    exc: Exception = None
+
+class TaskState:
+    name: str
+    author: str
+    startDateTime: datetime
+    programStr: str
+    argsStr: str
+    localProgram: bool
+    localProgramSrcDir: str
+    copyFiles: bool
+    copiedFilesList: list[str]
+    remoteWorkingDir: str
+    impersonateSysAdmin: bool
+    jobList: list[JobState]
+    overwriteExe: bool
+    overwriteFiles: bool
+    cleanupExeAfterCopy: bool
+    cleanupFilesAfterCopy: bool
+    timeout: int
 
 class AppState:
     domainName: str
     hostComputer: str
     aDSession: ADSession
-    
+    runningTasks: list[TaskState]
+    completedTasks: list[TaskState]
 
 appState = AppState()
